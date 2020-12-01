bf35915f0ea00348367dccf13a99c777011eb293
emanuel92 <wille.emanuel@gmail.com>
2020-12-01 21:49:16 +0100

    Add Header, Pages & Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpsoo-zox7hqBLeEujgEEZMhTdBZhw9tw",
+    authDomain: "reddit-clone-cbffe.firebaseapp.com",
+    databaseURL: "https://reddit-clone-cbffe.firebaseio.com",
+    projectId: "reddit-clone-cbffe",
+    storageBucket: "reddit-clone-cbffe.appspot.com",
+    messagingSenderId: "758518387317",
+    appId: "1:758518387317:web:dcc51d5f65667b16a39e3c",
+    measurementId: "G-GCG06R3FBZ"
+};
+
+export default firebaseConfig;
