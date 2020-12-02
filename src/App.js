30435004c6fdced9935cbbc6b5dcbb5ba2cb8b4e
emanuel92 <wille.emanuel@gmail.com>
2020-12-02 21:08:26 +0100

    Authentication Pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Redirect, Route } from 'react-router-dom';
 import { 
   IonApp,
   IonIcon,
-  IonLabel, 
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
@@ -22,6 +21,10 @@ import Trending from './pages/Trending';
 import Submit from './pages/Submit';
 import Search from './pages/Search';
 import Profile from './pages/Profile';
+import EditProfile from './pages/EditProfile';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Forgot from './pages/Forgot';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -48,12 +51,20 @@ const App = () => {
       <IonReactRouter>
         <IonTabs>
           <IonRouterOutlet>
-            <Route path="/" render={() => <Redirect to="/home" />} exact={true} />
-            <Route path="/home" component={Home} exact={true} />
+            <Route
+              path="/"
+              render={() => <Redirect to="/home" />} 
+              exact={true}
+            />
+            <Route path="/home" component={Home} />
             <Route path="/trending" component={Trending} />
             <Route path="/submit" component={Submit} />
             <Route path="/search" component={Search} />
             <Route path="/profile" component={Profile} />
+            <Route path="/edit-profile" component={EditProfile} />
+            <Route path="/register" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot" component={Forgot} />
             <Route component ={() => <Redirect to="/home" />} />
           </IonRouterOutlet>
           <IonTabBar slot="bottom">
